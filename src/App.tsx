88343fc95a79a9d51ba3e6388035ded7f3a81a1b
beMimg <bernardommguerreiro@gmail.com>
2024-06-24 18:15:41 +0200

    Create routing at App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/unauthenticated/Home";
+import Login from "./pages/unauthenticated/Login";
+import Register from "./pages/unauthenticated/Register";
+
 function App() {
-  return <p>d</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
