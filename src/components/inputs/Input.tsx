422a77dad9994a2f5c4704e4fa15ea35332262df
beMimg <bernardommguerreiro@gmail.com>
2024-06-24 18:18:22 +0200

    Create reusable input component

---
+++ src/components/inputs/Input.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IInput } from "../../interfaces/input.interface";
+
+const Input = ({ value, setter, placeholder }: IInput) => {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      onChange={(e) => setter(e.target.value)}
+      value={value}
+      className="w-full rounded-md border bg-black p-4 text-white placeholder:text-white focus:border-[3px] focus:border-sky-400 focus:outline-none"
+    />
+  );
+};
+
+export default Input;
