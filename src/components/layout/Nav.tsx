791d5a7a9e463373aca7e80bf4379d20d86f95e6
beMimg <bernardommguerreiro@gmail.com>
2024-06-26 18:48:31 +0200

    handle loading and error in Layout

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,22 +1,19 @@
-import React from "react";
 import { useUser } from "../../context/userContext";
+import { IUser } from "../../interfaces/User.interface";
+import ErrorText from "../feedback/ErrorText";
+import LoadingSpinner from "../feedback/LoadingSpinner";
 import NavLargeDevices from "../navegation/NavLargeDevices";
 
-const Nav = () => {
-  const { user, loading, error } = useUser();
-
+const Nav = ({ user }: IUser) => {
   return (
-    user && (
-      <nav className="w-full lg:h-full">
-        <div className="hidden h-full w-full lg:flex">
-          <NavLargeDevices user={user} />
-        </div>
-        <div className="absolute bottom-0 left-0 flex h-[60px] w-full lg:hidden">
-          <p>lol</p>
-          {/* <NavSmallDevices user={user} /> */}
-        </div>
-      </nav>
-    )
+    <nav className="w-full lg:h-full">
+      <div className="hidden h-full w-full lg:flex">
+        <NavLargeDevices user={user} />
+      </div>
+      <div className="absolute bottom-0 left-0 flex h-[60px] w-full lg:hidden">
+        {/* <NavSmallDevices user={user} /> */}
+      </div>
+    </nav>
   );
 };
 
--- src/pages/authenticated/Layout.tsx
+++ src/pages/authenticated/Layout.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../../components/layout/Nav";
+import { useUser } from "../../context/userContext";
+import LoadingSpinner from "../../components/feedback/LoadingSpinner";
+import ErrorText from "../../components/feedback/ErrorText";
 
 export const Layout = () => {
-  return (
+  const { user, loading, error } = useUser();
+
+  return user ? (
     <div className="h-screen bg-black text-white lg:flex lg:justify-center">
       <div className="flex h-full flex-col text-white lg:grid lg:w-[70%] lg:grid-cols-[25%,50%,25%]">
         <div className="w-full lg:h-full">
-          <Nav />
+          <Nav user={user} />
         </div>
         <div className="overflow-auto pb-[60px] lg:pb-0">
           <Outlet />
@@ -15,6 +20,13 @@ export const Layout = () => {
           <p>lol</p>
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="flex h-screen w-full items-center justify-center bg-black">
+      {loading && <LoadingSpinner size="50px" />}
+      {error && (
+        <ErrorText text="Something went wrong, please try again later" />
+      )}
     </div>
   );
 };
