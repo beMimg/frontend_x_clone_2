4949df00da3198ed39c4f289c732bef03ae07922
beMimg <bernardommguerreiro@gmail.com>
2024-06-26 18:24:40 +0200

    Create avatar component for profile pic

---
+++ src/components/layout/Avatar.tsx
@@ -0,0 +1,40 @@
+import { getDesiredColor } from "../../utils/getDesiredColor";
+
+const Avatar = ({
+  user,
+  size,
+}: {
+  user: { profile_color: string; profile_pic_src: string; first_name: string };
+  size: string;
+}) => {
+  const finalColor = getDesiredColor(user.profile_color);
+
+  return user.profile_pic_src ? (
+    <div className={`max-h-[${size}] max-w-[${size}] `}>
+      <img
+        src={user.profile_pic_src}
+        className={`flex rounded-full object-cover object-center`}
+        style={{
+          maxHeight: size,
+          minWidth: size,
+          maxWidth: size,
+          minHeight: size,
+        }}
+      />
+    </div>
+  ) : (
+    <div
+      className={`flex items-center justify-center rounded-full ${finalColor} `}
+      style={{
+        maxHeight: size,
+        minWidth: size,
+        maxWidth: size,
+        minHeight: size,
+      }}
+    >
+      {user.first_name.slice(0, 1).toUpperCase()}
+    </div>
+  );
+};
+
+export default Avatar;
