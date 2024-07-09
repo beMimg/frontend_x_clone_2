55e9d76f3879b82a65db9789d014df7d4a4b320e
beMimg <bernardommguerreiro@gmail.com>
2024-07-09 12:37:59 +0200

    Dashboard Header has same paddings as post

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -13,16 +13,16 @@ const DashboardHeader = ({
 }) => {
   return (
     <div className="flex w-full flex-col">
-      <div className="grid w-full p-2 py-4 lg:grid-cols-2">
+      <div className="grid w-full lg:grid-cols-2">
         <button className="border-b-2 border-sky-500 pb-1">For you</button>
         <button className="secondary-text hidden cursor-not-allowed lg:block">
           Following
         </button>
       </div>
-      <div className="flex w-full flex-col gap-2 border-y border-gray-700 p-2">
+      <div className="flex w-full flex-col gap-2 border-y border-gray-700 p-2 px-4">
         <div className="flex flex-row items-center justify-center gap-4">
           <div className="flex self-start">
-            <Avatar user={user} size="40px" />
+            <Avatar user={user} size="45px" />
           </div>
           <CreatePost setRerender={setRerender} close={false} />
         </div>
