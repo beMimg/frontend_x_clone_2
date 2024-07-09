0ad4e984dacec7825eeb9426b7247af2b6594a41
beMimg <bernardommguerreiro@gmail.com>
2024-07-09 12:33:52 +0200

    Post now uses flex instead of grid

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BASE_URL = "http://localhost:3000/api";
-export const BASE_URL = "https://backendxclone-production.up.railway.app/api";
+export const BASE_URL = "http://localhost:3000/api";
+// export const BASE_URL = "https://backendxclone-production.up.railway.app/api";
 
 export default axios.create({
   baseURL: BASE_URL,
